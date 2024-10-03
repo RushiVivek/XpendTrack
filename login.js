db57aa1a1d9090fa018258585db0528cda186fdb
Abhi-2765 <b.abhi2790@gmail.com>
2024-10-03 17:18:07 +0530

    Login Final Draft

---
+++ login.js
@@ -0,0 +1,25 @@
+let username = document.getElementById("email-inp");
+// let password = document.getElementById("password-inp");
+let password = document.getElementsByClassName("pwd");
+let invalids = document.getElementsByClassName("alert-sys");
+let alert_msg = document.getElementById("alert"); 
+
+function LoginHit() {
+  let email_input = username.value;
+  let pass_input = password.value;
+
+  if (email_input === "" || pass_input === "") {
+    alert_msg.textContent = "Error! Enter email and password";
+    if(invalids.style.display == none || invalids.style.display == "")
+      invalids.style.display = flex;
+  }
+}
+
+function showPassword(eleID) {
+  const x = document.getElementById(eleID);
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
